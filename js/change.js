c25b50cdcd0c284e2f762c53601f6138559fb08b
davidos_studi <david.ber0169@gmail.com>
2025-02-28 00:25:26 +0100

    Implémentation d'un message descriptif JS

--- js/change.js
+++ js/change.js
@@ -11,41 +11,66 @@ document.addEventListener("DOMContentLoaded", function() {
     
     // recuperer le bouton exercices
     const exercices = document.getElementById('exercices');
-
-    // recuperer la zone de message msg
     
     // recuperer la valeur du bouton exercices
     let exercice = 1;
     let script = document.getElementById('script');
-
-    // Récupere le nom du fichier JS.
-
-    // Changer de fichier JS entre 1 2 3 4 etc a 20
+    
+    // Ecouter les changement d'exercice
     exercices.addEventListener("change", function() {
+        
+        document.getElementById('msg').innerHTML = "test reussi";
 
-        // Initaliser la variable message qui contient le contenu du message
-        const msg = document.getElementById("exercices");
-        msg.innerHTML = "test reussi";
-    exercices.addEventListener('click', function () {
-        exercice = exercices.value;
+        // Initaliser la variable message
+        let msg = document.getElementById("exercices").innerHTML;
+
+        // Modifier le repertoire script.
+        exercice = Number(exercices.value);
         document.getElementById('script').src = `js/exo_${exercice}.js`;
-        if ( exercice === 1 ) {msg.innerHTML = "calculer le carré";}
-        if ( exercice === 2 ) {msg.innerHTML = "nombre pair";}
-        if ( exercice === 3 ) {msg.innerHTML = "nombre premier";}
-        if ( exercice === 4 ) {msg.innerHTML = "exo 4";}
-        if ( exercice === 5 ) {msg.innerHTML = "exo 5";}
-        if ( exercice === 6 ) {msg.innerHTML = "exo 6";}
-        if ( exercice === 7 ) {msg.innerHTML = "exo 7";}
-        if ( exercice === 8 ) {msg.innerHTML = "exo 8";}
-        if ( exercice === '9' ) {msg.innerHTML = "exo 9";}
-
+        
+        // Changer le nom du message
+        if ( exercice === 1 ) {document.getElementById('msg').innerHTML = "calculer le carré";}
+        if ( exercice === 2 ) {document.getElementById('msg').innerHTML = "afficher la somme";}
+        if ( exercice === 3 ) {document.getElementById('msg').innerHTML = "nombre pairs";}
+        if ( exercice === 4 ) {document.getElementById('msg').innerHTML = "nombre premier";}
+        if ( exercice === 5 ) {document.getElementById('msg').innerHTML = "table de multiplication";}
+        if ( exercice === 6 ) {document.getElementById('msg').innerHTML = "factorielle";}
+        if ( exercice === 7 ) {document.getElementById('msg').innerHTML = "son inverse";}
+        if ( exercice === 8 ) {document.getElementById('msg').innerHTML = "plus grand diviseur";}
+        if ( exercice === 9 ) {document.getElementById('msg').innerHTML = "plus petit diviseur";}
+        if ( exercice === 10 ) {document.getElementById('msg').innerHTML = "pgcd avec 10";}
+        if ( exercice === 11 ) {document.getElementById('msg').innerHTML = "ppcm avec 10";}
+        if ( exercice === 12 ) {document.getElementById('msg').innerHTML = "plus grand commun diviseur de ces deux nombres";}
+        if ( exercice === 13 ) {document.getElementById('msg').innerHTML = "plus petit commun multiple de ces deux nombres";}
+        if ( exercice === 14 ) {document.getElementById('msg').innerHTML = "suite de Fibonacci jusqu'à ce nombre";}
+        if ( exercice === 15 ) {document.getElementById('msg').innerHTML = "suite de Syracuse jusqu'à ce nombre";}
+        if ( exercice === 16 ) {document.getElementById('msg').innerHTML = "suite de Collatz jusqu'à ce nombre";}
+        if ( exercice === 17 ) {document.getElementById('msg').innerHTML = "suite de Lucas jusqu'à ce nombre";}
+        if ( exercice === 18 ) {document.getElementById('msg').innerHTML = "suite de Pell jusqu'à ce nombre";}
+        if ( exercice === 19 ) {document.getElementById('msg').innerHTML = "suite de Fibonacci généralisée jusqu'à ce nombre";}
+        if ( exercice === 20 ) {document.getElementById('msg').innerHTML = "suite de Lucas généralisée jusqu'à ce";}
         alert(`
                 Exercice ${exercice}\n
                 script ${script.src}\n
                 msg ${msg};
+                
          `);
-        console.log("msg", msg);
-    })
+        // PROBLEME 1
+        // dans la condition IF, exercice n'est pas un entier
+        // SOLUTION 1
+        // voir le type de variable
+        // console.log( typeof exercice );
+        // puis convertir exercice en un entier
+        
+            console.log(`
+                msg= ${ msg }
+                msg typeof = ${ typeof msg }\n
+                exercice = ${ exercice }
+                exercice typeof = ${ typeof exercice }
+               
+        `)
 
     })
+    // ON CLICK LISTENER
 })
+//DOM LOADED
