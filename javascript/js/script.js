f4bfc24eb8ac350c4fa04faa204568bf90847e42
davidos_studi <david.ber0169@gmail.com>
2025-02-28 01:31:27 +0100

    BOT AUTOMATION~

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -26,6 +26,20 @@ const minus = document.getElementById('minus');
 const plus = document.getElementById('plus');
 const power = document.getElementById('power');
 
+// Decocher toutes les checkbocses
+multiply.checked = false
+divide.checked = false
+plus.checked = false
+minus.checked = false
+power.checked = false
+
+// recuperer l'etat des checkbocses
+let multiply_var = multiply.checked;
+let divide_var = divide.checked;
+let minus_var = minus.checked;
+let plus_var = plus.checked;
+let power_var = power.checked;
+
 // initier a zero les deux nombres de l'utilisateur
 let mario = 0;
 let luigi = 0;
@@ -41,6 +55,7 @@ const _toad = document.getElementById('toad');
 // Ecoute les changement d'etats TRUE ou FALSE de chaque checkbox.
 // CHECKBOX MULLTPLIER
 multiply.addEventListener('change', function () {
+    multiply_var = multiply.checked;
 
     // Afficher le changement d'état.
     console.log("multiplier=", multiply_var);
@@ -48,6 +63,7 @@ multiply.addEventListener('change', function () {
 
 // CHECKBOX DIVISER
 divide.addEventListener('change', function () {
+    divide_var = divide.checked;
 
     // Afficher le changement d'état.
     console.log("diviser=", divide_var);
@@ -55,6 +71,7 @@ divide.addEventListener('change', function () {
 
 // CHECKBOX ADDITIONNER
 plus.addEventListener('change', function () {
+    plus_var = plus.checked;
 
     // Afficher le changement d'état.
     console.log("additionner=", plus_var);
@@ -62,6 +79,7 @@ plus.addEventListener('change', function () {
 
 // CHECKBOX SOUSTRAIRE
 minus.addEventListener('change', function () {
+    minus_var = minus.checked;
 
     // Afficher le changement d'état.
     console.log("soustraire=", minus_var);
@@ -69,6 +87,7 @@ minus.addEventListener('change', function () {
 
 // CHECKBOX AU CARRE
 power.addEventListener('change', function () {
+    power_var = power.checked;
 
     // Afficher le changement d'état.
     console.log("au carré", power_var);
@@ -86,7 +105,7 @@ peach.addEventListener('change', function () {
 
 // Faire le calcul choisi par l'utilisateur d'apres
 // les nombres choisi ains que l'operation
-const math = document.getElementById('msg');
+const math = document.getElementById('dodamathbutton');
 math.addEventListener('click', function () {
     
     // Verifier quelle operation a ete choisie par l'utilisateur
@@ -120,15 +139,17 @@ math.addEventListener('click', function () {
         _result = mario * mario;
         console.log("resultat = ", _result);
     }
+    
+
 
     // Derniere etape Afficher le resultat
     // Faire apparaitre l'element div en changeant la classe
     _toad.classList.remove('hide');
-    document.getElementById("toad").innerHTML=_result;
+    document.getElementById("toad").innerHTML = _result;
 })
 
 // Afficher l'entiereté des variables
-const click_here = document.getElementById('exercices');
+const click_here = document.getElementById('click_here');
 click_here.addEventListener('click', function () {
     console.log("multiplier=", multiply_var);
     console.log("diviser=", divide_var);
