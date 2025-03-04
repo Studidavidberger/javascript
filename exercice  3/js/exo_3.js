55e637b6a11a642aff0b2537c3ad6511e19c7693
davidos_studi <david.ber0169@gmail.com>
2025-03-04 10:59:40 +0100

    BOT AUTOMATION~

--- exercice  3/js/exo_3.js
+++ exercice  3/js/exo_3.js
@@ -4,30 +4,39 @@
 *
 * START
 *
-* declarer les boutons buttons, et calc.
+* Declarer la variable number, index, et result.
 *
-* declarer les variables number, et result
+* index = 0
 *
-* number  = INPUT = "Entrez votre nombre"
+* Declarer les boutons button, et calc
+*
+* number = INPUT "Entrez votre nombre"
 *
 * IF calc is clicked
-*   WHILE result is less than number
-*   CREATE a new div
-*   result = number
+*   result = index mod 10
+*   WHILE index is less of equal to number
+* {
+*      IF  = 0, 2, 4, 6, ou 8
+*           PRINT index
+*           index = index + 1
+*       ELSE
+*           index = index + 1
+* }
 *
 *
 * * */
  
-// Declarer les elements HTML
-// button, button2, debug, calc.
+// Declarer les elements HTML button, et calc.
 const button = document.getElementById('button');
-const debug = document.getElementById('debug');
 const calc = document.getElementById('calc');
+
+// Déclarer les elements HTML secondaire
 const version = document.getElementById('version');
+const debug = document.getElementById('debug');
 
 // Déclarer les variables number, et result.
 let number = 0;
-let result = 0;
+let index = 0;
     
 // RECUPERER LA VALEURE DE LA VARIABLE
     button.addEventListener('change', function () {
@@ -37,25 +46,22 @@ let result = 0;
         document.getElementById('version').innerHTML = document.getElementById('script').src;
     })
     
-// CALCULER LE CARRE.
+// CALCULER TOUT LES NOMBRES PAIRS..
     calc.addEventListener('click', function () {
-        result = number * number;
-        console.log("Le resultat est", result);
+        index = number % 10;
+        while ( index <= number ) {
+            if ( index )
+            console.log( number );
+
+        }
+
+
 
         // AFFICHER LE RESULTAT
         output.classList.remove('hide');
         document.getElementById("output").innerHTML = result;
     })
 
-// CALCULER LE CARRE.
-    calc.addEventListener('click', function () {
-        result = number * number;
-        console.log("Le resultat est", result);
-
-        // AFFICHER LE RESULTAT
-        output.classList.remove('hide');
-        document.getElementById("output").innerHTML = result;
-    })
 
 debug.addEventListener('click', function () {
     console.log(`
